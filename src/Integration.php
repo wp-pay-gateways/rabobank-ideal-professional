a26b65080e862e036855ce673b00a3e66897da59
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:48:16 +0100

    Extend from iDEAL Advanced library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_Rabobank_IDealAdvanced_Integration {
+class Pronamic_WP_Pay_Gateways_Rabobank_IDealAdvanced_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'rabobank-ideal-professional';
 		$this->name       = 'Rabobank - iDEAL Professional';
 		$this->provider   = 'rabobank';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Gateway';
 	}
 }
